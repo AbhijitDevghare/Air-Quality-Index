c47bd589ebcea95038ae331acdcdaf4c4036128b
AbhijitDevghare <kishordevghare6@gmail.com>
2025-11-26 17:38:40 +0530

    Some fixes 1

--- Frontend/air-quality-index-app/src/App.jsx
+++ Frontend/air-quality-index-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import CustomRoutes from "./routes/CustomRoutes"
 import { Provider } from "react-redux"
 import store from "./redux/store"
+import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Provider store={store}>
         <BrowserRouter>
           <CustomRoutes/>
+          <Toaster/>
         </BrowserRouter>
       </Provider>
         
